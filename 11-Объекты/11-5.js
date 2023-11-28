9b0832b2c29e53b68d271e9682281429081884ff
AnnaDvortsova <anna.dvortsova26@yandex.ru>
2023-11-28 20:56:49 +0300

    Решение задачи 11.6

---
+++ 11-Объекты/11-5.js
@@ -0,0 +1,45 @@
+// ***Задача 11.5***
+
+// Дан объект. Написать функцию, которая будет возвращать новый объект, в котором имена и значения свойств поменяются местами. Для решения задачи потребуется метод объектов, который в практикуме не описывался, его можно найти в документации.
+
+// input
+const dog = {
+    name: "Рекс",
+    age: 3,
+    home: "Москва",
+};
+
+// output
+// const dog = {
+// 	"Рекс": "name",
+// 	"3": "age",
+// 	"Москва": "home",
+// };
+
+// function revers(object) {
+//     const array = Object.entries(object);
+//     let newObject = {};
+//     array.forEach((element) => newObject[element[1]] = element[0]);
+
+//     // for (let i = 0; i < array.length; i++) {
+//     //     const elem = array[i];
+//     //     newObject[elem[1]] = elem[0];
+//     // }
+//     return newObject;
+// }
+
+// console.log(revers(dog));
+
+function revers(object) {
+    const array = Object.entries(object);
+    const array1 = array.map((elem) => {
+        const newArray = [elem[1], elem[0]];
+        return newArray;
+    });
+
+    const newObject = Object.fromEntries(array1);
+
+    return newObject;
+}
+
+console.log(revers(dog));
---
+++ 11-Объекты/11-6.js
@@ -0,0 +1,59 @@
+// ***Задача 11.6***
+
+// Даны объект - девайс (шлагбаум) и массив объектов - камер. Написать функцию, которая будет принимать этот объект и массив объектов, и возвращать объект со свойствами: название барьера `name` и массивом объектов с данными о камерах `cameras`. Свойство `cameras` должно содержать данные только о тех камерах, чьи `id` указаны в свойстве `cameraIDs`.
+
+// input
+const devices = {
+    name: "Barrier 1",
+    cameraIDs: [1, 2],
+};
+
+const cameras = [
+    {
+        id: 1,
+        name: "Camera 1",
+    },
+    {
+        id: 2,
+        name: "Camera 2",
+    },
+    {
+        id: 3,
+        name: "Camera 3",
+    },
+];
+
+// output
+// {
+//     name: "Barrier 1",
+//         cameras: [
+//             {
+//                 id: 1,
+//                 name: "Camera 1",
+//             },
+//             {
+//                 id: 2,
+//                 name: "Camera 2",
+//             },
+//         ],
+//  }
+
+function camerasName(devices, cameras) {
+    const devicesArray = devices.cameraIDs;
+    let newObject = {};
+    let newArray = devicesArray.map((elementDevicesArray) => {
+        let objectCameras = {};
+        const names = cameras.find((name) => name.id === elementDevicesArray);
+
+        objectCameras.id = names.id;
+        objectCameras.name = names.name;
+
+        return objectCameras;
+    });
+
+    newObject.name = devices.name;
+    newObject.cameras = newArray;
+    return newObject;
+}
+
+console.log(camerasName(devices, cameras));
